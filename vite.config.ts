d191ed0ef593920325345c115bf5a6aaab5acac6
Julien Gilbert <juliengilbert.dev@gmail.com>
2024-08-05 17:34:39 +0700

    Initiate project

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/Time-tracking-dashboard/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    sourcemap: true,
+    assetsInlineLimit: 0, // Ceci forcera Vite à toujours créer un fichier séparé pour les assets
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
